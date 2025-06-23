aab7d727d5738fbe39f671b3e571cb82a11bf5d7
Rajat Sachdeva <r.sachdeva3105@gmail.com>
2025-06-23 11:00:55 -0400

    problem 328

---
+++ 328_Odd_Even_Linked_List.js
@@ -0,0 +1,21 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (!head || !head.next) return head;
+
+  let odd = head;
+  let even = odd.next;
+  let evenHead = even;
+
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+  return head;
+};
