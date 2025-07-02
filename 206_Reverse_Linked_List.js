8f7aead1374021b3afa8a29610859056a51a5ab4
Rajat Sachdeva <r.sachdeva3105@gmail.com>
2025-07-02 14:44:49 -0400

    problem 206

---
+++ 206_Reverse_Linked_List.js
@@ -0,0 +1,14 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let node = null;
+  while (head) {
+    const temp = head.next;
+    head.next = node;
+    node = head;
+    head = temp;
+  }
+  return node;
+};
