7cc8646b2addeca8ccae405208be935a727ac153
Rajat Sachdeva <r.sachdeva3105@gmail.com>
2026-06-04 09:51:33 -0400

    problem 374

---
+++ 374_Guess_Number_Higher_or_Lower.js
@@ -0,0 +1,25 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+var guessNumber = function (n) {
+  let start = 1,
+    end = n;
+  while (start <= end) {
+    const mid = Math.floor(start + (end - start) / 2);
+    const result = guess(mid);
+    if (result === 1) start = mid + 1;
+    else if (result === -1) end = mid - 1;
+    else return mid;
+  }
+};
